9d88e30e97ad0e9215c2b9cdce44bb9e1388cff5
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-11-28 14:00:56 -0800

    program that prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ last_digit = number % 10 if number >= 0 else (abs(number) % 10) * -1
 if last_digit > 5:
     msg = 'greater than 5'
 elif last_digit == 0:
-    msg = 'zero'
+    msg = '0'
 else:
     msg = 'less than 6 and not 0'
 
