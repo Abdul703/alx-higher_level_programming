24765cce862eb4c91ee722ff73d28bbe7bf6db58
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-11-28 15:38:09 -0800

    program that print fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for num in range(100):
+        if num % 5 == 0 and num % 3 == 0:
+            print('FizzBuzz', end=' ')
+        elif num % 5 == 0:
+            print('Buzz', end=' ')
+        elif num % 3:
+            print('Fizz', end=' ')
+        else:
+            print(num, end=' ')
