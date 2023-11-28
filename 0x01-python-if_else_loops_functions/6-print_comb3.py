ae83222e228f87a43f71d067d60bd19f322088d7
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-11-28 14:54:59 -0800

    remove numbers that are thesame like 11, 33, ...

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 
 for num in range(99):
 
-    if num <= int(str(num)[::-1]):
-        if num != 0:
+    if num <= int(str(num)[::-1]) and num % 10 != num // 10:
+        if num != 1:
             print(', ', end='')
         print("{:02d}".format(num), end='')
 print()
