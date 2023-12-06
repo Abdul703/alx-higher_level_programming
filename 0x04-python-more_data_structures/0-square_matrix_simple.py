4c95102c75cd9feb822a4a36e6bdd03fe347a1d8
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-12-06 17:17:18 +0100

    removed lambda assignment

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    func = lambda x: x**2
-    return [list(map(func, row)) for row in matrix]
+    return [list(map(lambda x: x**2, row)) for row in matrix]
