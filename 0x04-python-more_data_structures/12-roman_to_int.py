21c63360798bdd27a293f6a51a01a15d5b3c55bc
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-12-06 20:18:09 +0100

    roman to integer converter

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    romans = {
+        'I': 1,
+        'IV': 4,
+        'V': 5,
+        'IX': 9,
+        'X': 10,
+        'XL': 40,
+        'L': 50,
+        'XC': 90,
+        'C': 100,
+        'CD': 400,
+        'D': 500,
+        'DM': 900,
+        'M': 1000,
+    }
+    num = 0
+
+    for figure in sorted(romans.keys(), key=len, reverse=True):
+        while figure in roman_string:
+            roman_string = roman_string.replace(figure, '', 1)
+            num += romans[figure]
+    return num
