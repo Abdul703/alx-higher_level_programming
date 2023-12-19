0b43ccf157721b0910bd5cdfe0f905f00b9a33ce
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-12-20 00:20:34 +0100

    added function that print square shape

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+A module for square-related operations.
+"""
+
+
+class Square:
+    """
+    a class representing square.
+
+    Attributes:
+    - __size (int): The size of the square (private).
+    """
+
+    def __init__(self, size=0):
+        """
+        Initiate the size of the square object.
+
+        Args:
+            size (int): size of the square object created.
+        """
+
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        compute the area of the square
+
+        Returns:
+            int: area of the square
+        """
+        return self.__size**2
+
+    @property
+    def size(self):
+        """
+        getter for the size
+
+        Returns:
+            int: the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the value of the size to new value
+
+        Args:
+            value (int): new value for the size
+        """
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        elif value < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """
+        print square shape using #
+        """
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end='')
+            print()
