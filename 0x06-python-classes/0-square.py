0b8b76d2ba3cc6068f9eda0d9a5a37f985f01443
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-12-19 23:43:54 +0100

    added docstring for module

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+A module for square-related operations.
+"""
 
 class Square:
-    """a class representing square
+    """
+    a class representing square
     """
     pass
