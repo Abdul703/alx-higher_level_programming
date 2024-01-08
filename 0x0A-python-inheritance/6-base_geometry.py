e9a6dad52cf7e07b5e204973e52f68bab5cd9d6f
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-01-09 00:29:44 +0100

    fixed style issue

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,7 +6,7 @@ Module that provides a geometry class
 
 class BaseGeometry:
     """A geometry class"""
-    
+
     def area(self):
         """area of the provided shape"""
         raise Exception('area() is not implemented')
