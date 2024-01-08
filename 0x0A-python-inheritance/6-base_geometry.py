56faabb69deed1d9b943b10e7b4f2ffbbbedc270
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-01-09 00:29:05 +0100

    added area function

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module that provides a geometry class
+"""
+
+
+class BaseGeometry:
+    """A geometry class"""
+    
+    def area(self):
+        """area of the provided shape"""
+        raise Exception('area() is not implemented')
