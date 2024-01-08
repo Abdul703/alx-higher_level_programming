d7c5e6bb6bc4546d372defce5555719a347247ef
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-01-08 12:18:02 +0100

    added function that returns attrs and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that provide function to returns attributes and methods of an object
+"""
+def lookup(obj):
+    """returns attributes and methods of an object
+
+    Args:
+        obj (object): object to list its attributes and methods
+
+    Returns:
+        list: ttributes and methods of an object
+    """
+    return dir(obj)
