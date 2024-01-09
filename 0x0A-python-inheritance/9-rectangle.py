f3a87ee6a1adb87d120abe74a6603118b3a779ca
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-01-09 17:05:18 +0100

    import Rectangle class

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,30 +2,7 @@
 """
 Module that provides a geometry classes
 """
-
-
-class BaseGeometry:
-    """A geometry base class"""
-
-    def area(self):
-        """area of the provided shape"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """validate value
-
-        Args:
-            name (str): name of the value
-            value (int): the integer value of name
-
-        Raises:
-            TypeError: if value is not an int
-            ValueError: value is less than or equal to 0
-        """
-        if type(value) != int:
-            raise TypeError(f'{name} must be an integer')
-        if value <= 0:
-            raise ValueError(f'{name} must be greater than 0')
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
