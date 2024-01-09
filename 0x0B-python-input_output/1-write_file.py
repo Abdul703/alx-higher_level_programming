64336dbb8bb9f5b7dd1759dd7239364c9dbf6d22
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-01-10 00:53:20 +0100

    function that write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python I/O module"""
+
+
+def write_file(filename="", text=""):
+    """write to a file
+
+    Args:
+        filename (str, optional): name of the file. Defaults to "".
+        text (str, optional): text to write. Defaults to "".
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
