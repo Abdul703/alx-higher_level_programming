03a9835216170a797b95b7c2f7263b63235772ee
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-01-09 16:33:27 +0100

    otimize the code

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,5 +11,4 @@ class MyList(list):
 
     def print_sorted(self):
         """Prints the list in ascending order."""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
