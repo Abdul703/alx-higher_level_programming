c0c21630c1e34d9acb892dc11ea31fc558d45acf
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-01-09 01:24:58 +0100

    validate before assigning variables

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -41,7 +41,7 @@ class Rectangle(BaseGeometry):
             width (int): width of the rectangle
             height (int): height of the rectangle
         """
+        self.integer_validator('width', width)
         self.__width = width
+        self.integer_validator('height', height)
         self.__height = height
-        self.integer_validator('width', self.__width)
-        self.integer_validator('height', self.__height)
