0d02bb84855d28093dcd4c9bad1cea73682fa85f
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-01-14 19:25:50 +0100

    update and str function added

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,3 +30,44 @@ class Square(Rectangle):
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
     
+    @property
+    def size(self):
+        """Getter for the size
+
+        Returns:
+            int: rectangle size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the rectangle
+
+        Args:
+            value (int): value of the size
+
+        Raises:
+            TypeError: value is not integer
+            ValueError: value is less than or equal to 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+        self.__height = value
+        self.__size = value
+
+    def update(self, *args, **kwargs):
+        """Update Square Attributes"""
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+
+        elif kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+    
