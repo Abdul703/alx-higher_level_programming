6853fa9dfdebe046b4c2adaae3e108329dbc687f
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-01-15 22:26:17 +0100

    added __init__.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Base class for all subsequent classes"""
-
+import json
 
 class Base:
     """The Base class"""
@@ -18,3 +18,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """serialize an object
+
+        Args:
+            list_dictionaries (dict): an object to serialize
+
+        Returns:
+            str: serialized object
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        return json.dumps(list_dictionaries)
