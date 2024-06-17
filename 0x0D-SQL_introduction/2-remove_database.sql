f0e2c33da7125409be0cb6bb13b856ac017f8885
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-17 17:04:55 +0100

    script that delete database

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- script that deletes the database hbtn_0c_0 in MySQL server.
+-- If the database hbtn_0c_0 does not exists, the script will not fail
+
+-- deleting database
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;
