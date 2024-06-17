d38d99c9a84b64b92e37295c5461c41c0d5f8bf5
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-17 22:25:19 +0100

    sql writing style updated

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,6 @@
 -- script that lists all records of the table second_table of a database in MySQL server.
 -- Records is ordered by score (top first)
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
