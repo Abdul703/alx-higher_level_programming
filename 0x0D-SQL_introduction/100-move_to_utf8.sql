ae1ea935da485924f78f018672023b45a4d26f17
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-21 16:21:53 +0100

    change database charset

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- script that converts hbtn_0c_0 database, Table first_table and Field name in first_table to UTF8
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8;
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4  COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8;
