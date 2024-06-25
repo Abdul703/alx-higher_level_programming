71ee6c8a527588c7f2775cb4d3bd412feb810a6c
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-06-25 01:03:51 +0100

    script that create new database and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- script that creates the database hbtn_0d_usa and the table state
+
+-- creating database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+-- creating state table
+CREATE TABLE IF NOT EXISTS states(
+    id INT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
